95a9eaaf6245b269e50ece5ed4790437773c0f73
valen lago <vlago@itba.edu.ar>
2018-01-23 02:06:28 -0300

    Modificaciones en Unit.h

--- SkirmishWars/Unit.h
+++ SkirmishWars/Unit.h
@@ -2,6 +2,11 @@
 
 #include "Map.h"
 
+
+#include <string>
+using namespace std;
+
+
 //----- UNIT TYPE [CODES] -----
 #define INFANTRIES	"in"#x
 #define MECHS		"me"#x
@@ -13,18 +18,57 @@
 #define TANKS		"ta"#x
 #define MEDTANKS	"mt"#x
 
+//enum unit{infantry, mech, rocket, recon, APC, antiair, artillery, tank, medtank }unitType;
+
+// Tabla de terrein defense modifiers!!!!!!!
+
+
+struct shape{
+	int moon;
+	int star;
+	int square;
+	int triangle;
+	int circle;
+};
+//moverse sobre HQ y buildings cuesta 1MP... 
+struct terrein {
+	int grass;
+	int street;
+	int tree;
+	int water;
+	int mountain;
+};
+
 class Unit
 {
 public:
 	Unit();
 	~Unit();
+
 	//Attack Unit
 	//Attack Building
 	//Move
 	//Defend
 
 private:
+
+	shape unitShape;	// moon, star, square, triangle, circle
+	string unitType;   // foot, wheel, tread  
 	TeamColor team;
-	int health;
-	int attackDamage;
+
+	int cost;
+
+	int HP;  // HealthPoints 
+	//int attackDamage;
+	int defense; // Puntos de defensa de la unidad
+
+	int MP;  // Movement Points
+	int Range; // Rango de alcance para el ataque
+
+	shape FP; // FirePoints para la unidad SIN reducir
+	shape FPreduced; // FirePoints para la unidad reducida (si HP<5 la unidad esta reducida)
+
+	terrein terreinMovementCost;
+
+
 };
