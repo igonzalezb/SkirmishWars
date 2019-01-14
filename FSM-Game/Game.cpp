ac5ec5a2be823ac5478aba0924c4a56c8e2e260d
malenamuller <mmuller@itba.edu.ar>
2019-01-14 11:52:51 -0300

    empezando capturing properties

--- FSM-Game/Game.cpp
+++ FSM-Game/Game.cpp
@@ -11,3 +11,24 @@ Game::~Game()
 {
 //destruir los players;
 }
+
+
+void Game::IcaptureProperty()
+{
+	if (((myMap->getTile(attacker.i, attacker.j)->getUnit()->getTeam()) == (playerMe->getTeam())) &&
+		(((myMap->getTile(attacker.i, attacker.j)->getUnit()->getType().compare("infantry")) == 0) || //VER si esto y la linea de abajo estan bien o no
+		((myMap->getTile(attacker.i, attacker.j)->getUnit()->getType().compare("mech")) == 0))&&
+		((myMap->getTile(defender.i,defender.j)->getBuilding())!= NULL))
+	{
+
+	}
+}
+
+string building("city");
+int iguales = building.compare("city");
+
+/////////////////////////////////// PASAR LAS SIGUIENTES FUNCIONES ACA
+void setAttacker();
+void setDefender();
+coordenadas getAttacker();
+coordenadas getDefender();
