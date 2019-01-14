17c806b15878400833548627baee63ff3823fa98
malenamuller <mmuller@itba.edu.ar>
2019-01-14 12:54:47 -0300

    funcion captureProperty terminada, falta chequearla

--- FSM-Game/Game.cpp
+++ FSM-Game/Game.cpp
@@ -13,14 +13,28 @@ Game::~Game()
 }
 
 
-void Game::IcaptureProperty()
+void Game::captureProperty(Player* pAttacker, Player* pDefender)
 {
-	if (((myMap->getTile(attacker.i, attacker.j)->getUnit()->getTeam()) == (playerMe->getTeam())) &&
+	if (((myMap->getTile(attacker.i, attacker.j)->getUnit()->getTeam()) == (pAttacker->getTeam())) && //ACA PONER PLAYER ATTACKER
 		(((myMap->getTile(attacker.i, attacker.j)->getUnit()->getType().compare("infantry")) == 0) || //VER si esto y la linea de abajo estan bien o no
 		((myMap->getTile(attacker.i, attacker.j)->getUnit()->getType().compare("mech")) == 0))&&
-		((myMap->getTile(defender.i,defender.j)->getBuilding())!= NULL))
+		((myMap->getTile(defender.i,defender.j)->getBuilding())!= NULL)&&
+		((myMap->getTile(defender.i, defender.j)->getBuilding()->getTeam()) ==(pDefender->getTeam())))//ACA PONER PLAYER DEFENDER
 	{
-
+		if ((myMap->getTile(attacker.i, attacker.j)->getUnit()->getHp()) < 5)//Si la unidad que conquista esta reducida
+		{
+			myMap->getTile(defender.i, defender.j)->getBuilding()->setCp((myMap->getTile(defender.i, defender.j)->getBuilding()->getCp()) - 1);
+		}
+		else//Si la que conquista NO esta reducida
+		{
+			myMap->getTile(defender.i, defender.j)->getBuilding()->setCp((myMap->getTile(defender.i, defender.j)->getBuilding()->getCp()) - 2);
+		}
+	}
+	//HACER: Girar carta de la property o mostrar en algun  lado un contador con el valor nuevo del CP de la property atacada
+	if ((myMap->getTile(defender.i, defender.j)->getBuilding()->getCp())<=0)
+	{
+		//en el tile donde estaba antes el building del oponente, poner el mismo building pero de mi equipo.
+		myMap->getTile(defender.i, defender.j)->getBuilding()->setTeam(myMap->getTile(attacker.i, attacker.j)->getUnit()->getTeam());
 	}
 }
 
